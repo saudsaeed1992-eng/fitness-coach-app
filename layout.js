6a937da21f22810e41ca35e9257d91b274d91165
saudsaeed1992-eng <saudsaeed1992@gmail.com>
2026-05-23 00:49:29 +0400

    Update layout.js

--- layout.js
+++ layout.js
@@ -1,13 +1,6 @@
-import "./globals.css";
-
-export const metadata = {
-  title: "Fat Loss Coach",
-  description: "Mobile-first fitness coaching dashboard",
-};
-
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html>
       <body>{children}</body>
     </html>
   );
